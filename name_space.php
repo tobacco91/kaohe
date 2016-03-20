ab7f5d66c309c2cd48f19b140e8f3ed4a4220d48
tobacco91 <991243902@qq.com>
2016-03-20 14:58:57 +0800

    shiyan

---
+++ name_space.php
@@ -0,0 +1,19 @@
+<?php
+namespace A;
+class pikaqiu{
+	public function __construct(){
+		echo "pikaqiu";
+	}
+	public function qiu(){
+		echo 'qiu';
+	}
+}
+
+
+
+// namespace CAb;
+//   function ABC(){
+//  	echo "abc";
+//  }
+
+?>
